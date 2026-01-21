3db941030d8e78a0afbea6a507de39b10613d4df
lybertytour-jpg <lybertytour@gmail.com>
2026-01-21 22:52:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// папка со сборкой Vite
+// отдаём собранный фронт
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
