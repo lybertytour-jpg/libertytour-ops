7b241ec42cb6457f532886f4ad792c10309838f1
lybertytour-jpg <lybertytour@gmail.com>
2026-01-21 23:18:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// отдаём собранный фронт
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+log(`Server running on port ${PORT}`);
 });
